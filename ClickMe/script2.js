6ed862c13fc0ea870b7a2b18d23f46d96f9aebe9
BellaMrx <116973159+BellaMrx@users.noreply.github.com>
2022-12-04 19:38:06 +0100

    update js

---
+++ ClickMe/script2.js
@@ -0,0 +1,19 @@
+
+
+function clickButton() {
+    var winText1 = document.querySelector(".text1");
+    winText1.innerHTML = "Okay, you can.";
+
+    var winText2 = document.querySelector(".text2");
+    winText2.innerHTML = "Congratulations, you have won the realization that you like to waste your time with bullshit.";
+
+    var img = document.querySelector(".cup");
+    img.src = "CupYouwon.PNG";
+	img.style.width = "125px";
+	img.style.height = "200px";
+}
+
+
+
+
+
