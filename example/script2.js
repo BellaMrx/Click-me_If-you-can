c9afa328758f833116855a2359d30c37224982e8
BellaMrx <116973159+BellaMrx@users.noreply.github.com>
2022-12-07 16:51:50 +0100

    ud script

--- example/script2.js
+++ example/script2.js
@@ -68,29 +68,64 @@ function animateText(comments){
 animateText(0);
 
 
-const button = document.querySelector(".container-button button");
+// countdown "TRUST ME"
+    // The data/time we want to countdown to
+    var countDownDate = new Date("Jul 25, 2021 16:37:52").getTime();
 
-window.addEventListener("mousemove", (e) => {
-  button.addEventListener("mouseenter", () => {
-    if (textToAnimate.length) {           // egal ob textAI , textToAnimate.length oder btn
-      if (e.movementX > 0) {
-        if (e.pageX < window.innerWidth / 2) {
-          button.style.transform = `translateX(${e.pageX / 3}px)`;
-          button.style.transform = `translateY(${e.pageY / 3}px)`;      // Y-axes
-        } else {
-          button.style.transform = `translateX(-${e.pageX / 3}px)`;
-          button.style.transform = `translateY(-${e.pageY / 3}px)`;     // Y-axes
-        }
-      } else {
-        if (e.pageX < window.innerWidth / 2) {
-          button.style.transform = `translateX(${e.pageX / 3}px)`;
-        } else {
-          button.style.transform = `translateX(-${e.pageX / 3}px)`;
-        }
-      } 
-    } 
-  }   );      // dazwischen
-});
+    // Run myfunc every second
+    var myfunc = setInterval(function() {
+
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+        
+    // Calculating the days, hours, minutes and seconds left
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+        
+    // Result is output to the specific element
+    document.getElementById("days").innerHTML = days + "d "
+    document.getElementById("hours").innerHTML = hours + "h " 
+    document.getElementById("mins").innerHTML = minutes + "m " 
+    document.getElementById("secs").innerHTML = seconds + "s " 
+        
+    // Display the message when countdown is over
+    if (timeleft < 0) {
+        clearInterval(myfunc);
+        document.getElementById("days").innerHTML = ""
+        document.getElementById("hours").innerHTML = "" 
+        document.getElementById("mins").innerHTML = ""
+        document.getElementById("secs").innerHTML = ""
+        document.getElementById("end").innerHTML = "TRUST ME!";
+    }
+    }, 5000);
+
+
+    const button = document.querySelector(".container-button button");
+
+    window.addEventListener("mousemove", (e) => {
+      button.addEventListener("mouseenter", () => {
+        
+          if (e.movementX > 0) {
+            if (e.pageX < window.innerWidth / 2) {
+              button.style.transform = `translateX(${e.pageX / 3}px)`;
+              button.style.transform = `translateY(${e.pageY / 3}px)`;      // Y-axes
+            } else {
+              button.style.transform = `translateX(-${e.pageX / 3}px)`;
+              button.style.transform = `translateY(-${e.pageY / 3}px)`;     // Y-axes
+            }
+          } else {
+            if (e.pageX < window.innerWidth / 2) {
+              button.style.transform = `translateX(${e.pageX / 3}px)`;
+            } else {
+              button.style.transform = `translateX(-${e.pageX / 3}px)`;
+            }
+           
+        }  
+      }  );      // dazwischen
+    });
+
 
 
 
